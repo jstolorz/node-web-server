7863890f6c218ccc54d6143aad3f87cb9a542410
jstolorz <jstolorz@gmail.com>
2018-06-13 17:28:56 +0200

    Commit version with projects page

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        portfolio: 'This is page with projects portfolio.'
+    });
+});
+
 app.listen(port, () => {
    console.log(`Server is up on port: ${port}`);
 });
